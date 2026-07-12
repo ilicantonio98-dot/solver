0dfbad6586a8a15453544f1188ed58ee1dc5165b
ilicantonio98 <ilicantonio98@gmail.com>
2026-07-12 08:03:56 +0200

    Add memoization state key

---
+++ src/engine/state.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <array>
+#include <functional>
+
+
+namespace blackjack
+{
+
+
+struct StateKey
+{
+
+    int total;
+    bool soft;
+
+    std::array<int,13> shoe;
+
+
+    bool operator==(const StateKey& other) const
+    {
+        return total == other.total &&
+               soft == other.soft &&
+               shoe == other.shoe;
+    }
+
+};
+
+
+
+struct StateHash
+{
+
+    std::size_t operator()(const StateKey& s) const
+    {
+
+        size_t h =
+            std::hash<int>{}(s.total);
+
+
+        h ^=
+            std::hash<bool>{}(s.soft)
+            << 1;
+
+
+        for(auto c : s.shoe)
+        {
+            h ^=
+                std::hash<int>{}(c)
+                +
+                0x9e3779b9
+                +
+                (h<<6)
+                +
+                (h>>2);
+        }
+
+
+        return h;
+    }
+
+};
+
+
+}
